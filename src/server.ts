f1c582aaee9488f9538499eddea61b6cef7d551c
Hadryanns <hadryan2007nascimentosantos@gmail.com>
2025-09-27 20:38:59 -0300

    Teste

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+const server = Fastify()
+server.register(cors, {
+    origin: true
+})
+
+// Middleware para configurar CSP
+server.addHook('onSend', async (request, reply, payload) => {
+    reply.header('Content-Security-Policy', "default-src 'self'; connect-src 'self' https://ourchat-ii0z.onrender.com"); 
+    return payload;
+});
+
+
+server.patch("/teste",(request:any,response)=>{
+    return 
+})
+
+server.listen({
+    host:"0.0.0.0",
+    port: process.env.PORT ?? 3333,
+}as any);
